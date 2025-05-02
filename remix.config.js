0965415fe89d06256cb792e500d25bbbb2d752e7
zlzmm5188 <zlzmm5188@gmail.com>
2025-05-02 20:05:24 +0800

    Add files via upload

---
+++ remix.config.js
@@ -0,0 +1,8 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+  appDirectory: "app",
+  assetsBuildDirectory: "public/build",
+  publicPath: "/build/",
+  serverBuildPath: "build/index.js",
+  serverModuleFormat: "cjs",
+};
